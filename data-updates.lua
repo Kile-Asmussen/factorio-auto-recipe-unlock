e5b039ac705465851f8bb90947af81fc0deba5d4
Kile Asmussen <kile.asmussen@gmail.com>
2026-05-26 16:29:30 +0200

    .

--- data-updates.lua
+++ data-updates.lua
@@ -4,7 +4,7 @@ local auto_unlock = require 'auto-unlock'
 local all_recipes = {}
 
 for name, _ in pairs(data.raw.recipe) do
-    table_insert(all_recipes, name)
+    table.insert(all_recipes, name)
 end
 
 auto_unlock.auto_unlock_recipes(all_recipes)
